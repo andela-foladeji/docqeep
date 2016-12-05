0bdca3c468ed36e1d8d7b7a859f8550ec5820cf3
Femi Oladeji <femidotexe@gmail.com>
2016-12-05 22:32:11 +0100

    Test: Add test for roles endpoints

---
+++ test/server/roles.spec.js
@@ -0,0 +1,75 @@
+const app = require('../../server/index'),
+  request = require('supertest')(app),
+  assert = require('chai').assert,
+  db = require('../../server/models'),
+  fakeData = require('../fake-data');
+
+describe('Roles related activites', () => {
+  let roleId, token;
+  before((done) => {
+    db.role.create(fakeData.role1).then((role) => {
+      roleId = role.dataValues.id;
+      fakeData.user.roleId = roleId;
+      request.post('/users')
+        .send(fakeData.user)
+        .end((err, res) => {
+          if (!err) {
+            token = res.body.token;
+          }
+          done();
+        });
+    });
+  });
+
+  after((done) => {
+    db.sequelize.sync({ force: true })
+      .then(() => {
+        done();
+      });
+  });
+
+  describe('POST /role to create a new role', () => {
+    it('allows only an admin to create role', (done) => {
+      request.post('/role')
+        .set({ Authorization: token })
+        .send(fakeData.role2)
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.isTrue(res.body.done);
+          assert.isDefined(res.body.role);
+          done();
+        });
+    });
+
+    it('prevents a non admin user from creating a role', (done) => {
+      fakeData.user2.roleId = 2;
+      request.post('/users')
+        .send(fakeData.user2)
+        .end((err, res) => {
+          const user2Token = res.body.token;
+          request.post('/role')
+            .set({ Authorization: user2Token })
+            .send(fakeData.role2)
+            .end((err, res) => {
+              assert.equal(res.status, 401);
+              assert.isFalse(res.body.done);
+              assert.isUndefined(res.body.role);
+              done();
+            });
+        });
+    });
+  });
+
+  describe('GET /roles to get all created roles', () => {
+    it('gets all created roles if requested by admin', (done) => {
+      request.get('/role')
+        .set({ Authorization: token })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.isArray(res.body.roles);
+          assert.equal(res.body.roles.length, 2);
+          done();
+        });
+    });
+  });
+});
