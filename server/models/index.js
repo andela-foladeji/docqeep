3a413e80dde90ef1092e45817b3293cb84df6505
Femi Oladeji <femidotexe@gmail.com>
2016-12-09 01:51:44 +0100

    Code-Quality: Improve code quality

--- server/models/index.js
+++ server/models/index.js
@@ -13,11 +13,11 @@ config.db.password, config.db.options);
 
 fs
   .readdirSync(__dirname)
-  .filter((file) => {
-    return (file.indexOf('.') !== 0) &&
+  .filter(file =>
+    (file.indexOf('.') !== 0) &&
     (file !== basename) &&
-    (file.slice(-3) === '.js');
-  })
+    (file.slice(-3) === '.js')
+  )
   .forEach((file) => {
     const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
