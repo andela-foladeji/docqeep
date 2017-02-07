9d806d7a8e324cbfe15b2c016e1556036b262524
Femi Oladeji <femidotexe@gmail.com>
2017-02-07 02:36:55 +0100

    Improve UX for document display

--- client/src/components/presentational/DocumentDisplay.js
+++ client/src/components/presentational/DocumentDisplay.js
@@ -4,14 +4,37 @@ import { Link } from 'react-router';
 class DocumentDisplay extends Component {
   constructor() {
     super();
+    this.state = { pending: true };
   }
 
   componentWillMount() {
     this.props.getDoc();
   }
+  
+  componentWillReceiveProps(nextProps) {
+    this.setState({ pending: false });
+  }
 
   render() {
+    if(this.state.pending) {
+      return(
+        <div class="row">
+          <div class="valign-wrapper loader-wrapper">
+            <img class="loader valign" src="../img/loader.gif" />
+          </div>
+        </div>
+      );
+    }
     const allDocs = (this.props.doc.allDocuments) ? this.props.doc.allDocuments : [];
+    if(allDocs.length === 0) {
+      return(
+        <div class="row">
+          <div class="valign-wrapper loader-wrapper">
+            <img class="loader valign" src="../img/empty.jpg"/>
+          </div>
+        </div>
+      ); 
+    }
     return(
       <div class="row">
         {allDocs.map((tile) => (
